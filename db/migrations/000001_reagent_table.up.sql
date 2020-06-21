bab94cbe5425976f84a8ae3cd60379ebf097c6ff
Yage Hu <yagehu@qq.com>
2020-06-20 20:01:50 -0700

    Add more fields to reagent table

--- db/migrations/000001_reagent_table.up.sql
+++ db/migrations/000001_reagent_table.up.sql
@@ -1,3 +1,6 @@
 CREATE TABLE IF NOT EXISTS reagent (
-    id UUID PRIMARY KEY
+    id UUID PRIMARY KEY,
+    guid UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    deleted_at TIMESTAMP NULL
 );
